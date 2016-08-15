05d5358095969b5a81b21bd9a7238820549108d1
Alan Kebert <adkebert@yahoo.com>
2016-08-15 16:26:50 -0400

    adjusted formatting of the new and edit term pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 require_relative '../models'
 
-Term.create(name: "Glossolalia", definition: "To speak in tongues", author: "A Kebert")
+gloss = Term.create(name: "Glossolalia", definition: "To speak in tongues", author: "A Kebert")
+palin = Term.create(name: "Palindrome", definition: "A word or phrase that is identical forwards and backwards", author: "A Kebert")
+auto = Term.create(name: "Autobot", definition: "A heroic transforming robot that opposes the Decepticons", author: "A Kebert")
+decept = Term.create(name: "Decepticon", definition: "A malicious transforming robot that opposes the Autobots", author: "A Kebert")
+megatron = Term.create(name: "Megatron", definition: "The leader of the Decepticons", author: "A Kebert")
 
-Category.create(name: "Words about Words", subject_area: "Back End Engineering")
+words = Category.create(name: "Words about Words", subject_area: "Back End Engineering")
+bots = Category.create(name: "Transformers", subject_area: "Back End Engineering")
+
+gloss.categories = [words]
+palin.categories = [words]
+auto.categories = [bots]
+decept.categories = [bots]
+megatron.categories = [bots]
